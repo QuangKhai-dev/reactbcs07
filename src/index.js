caeb766029d1ded14c3d9bdb4c6adf92c9666e46
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2023-06-13 07:06:20 +0700

    khoi tao du an react va co ban

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Body from './Components/Body';
+import ThongTin from './DataBinding/ThongTin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    {/* <Header /> */}
+    {/* <Body /> */}
+    {/* <Footer /> */}
+    <ThongTin />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
